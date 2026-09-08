bf015f13406184982ca8dcdaf4d0b7dfa8c811dd
nafiz512 <nafizmuntasir512@gmail.com>
2026-09-08 22:37:11 +0600

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added Clerk dependencies to package.json for authentication.
    - Implemented ClerkProvider in the app layout to manage authentication state.
    - Created sign-in and sign-up pages with email verification.
    - Added auth layout to handle redirection based on authentication status.
    - Removed the about page and updated the index page to reflect changes.
    - Introduced a sign-out button component for user session management.
    - Created a home layout and page to welcome users based on their authentication status.

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,41 @@
-import { } from "expo-router";
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 import "../../global.css"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+// import { ClerkProvider } from '@clerk/expo'
+// import { tokenCache } from '@clerk/expo/token-cache'
+
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  return <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    <Stack></Stack>
-  </ThemeProvider>;
+  const insets = useSafeAreaInsets();
+
+  return <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+          }
+        }}>
+        <Stack.Screen name="(home)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+        {/* <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
+
+      </Stack>
+    </ThemeProvider>
+  </ClerkProvider>;
 }
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -4,10 +4,8 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View >
-      <Text className="text-white text-2xl">Ok thats fine</Text>
-      <Link href={'/about'}>
-        <Text style={{ color: 'white' }}>About</Text>
-      </Link>
+      <Text className="font-bold dark:text-white text-2xl">Ok thats fine</Text>
+      {/* <Link href={'/(home)'}><Text className="dark:text-white text-black"> Home</Text></Link> */}
     </View>
   );
 }
